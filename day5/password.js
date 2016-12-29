9d53ad529174f3169876acd2dbb6a1fc130d823b
Rigo <I831003@global.corp.sap>
2016-12-29 10:43:10 -0200

    implemented day5 part 2

--- day5/password.js
+++ day5/password.js
@@ -23,10 +23,27 @@ var day5 = function() {
 
 var day5part2 = function() {
   for (var i = 0; i < input.length; i++) {
-
+    var password = '        '
+    var prefix = input[i]
+    var index = 0
+    var replaceChar = function (str, ch, pos) {
+      var arr = str.split('')
+      arr[pos] = ch
+      str = arr.join('')
+      return str
+    }
+    while (password.replace(/\s/g, '').length < 8) {
+      var hash = ''
+      while (!hash.startsWith('00000')) hash = md5(prefix + index++)
+      var pos = Number(hash.charAt(5))
+      if (isNaN(pos) || pos > 7 || password.charAt(pos) != ' ') continue;
+      var letter = hash.charAt(6)
+      password = replaceChar(password, letter, pos)
+      // console.log(password, index-1, hash)
+    }
     $('#day5part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(password)
       .append('<br>')
   }
 
