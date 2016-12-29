a9c413407b0fc379bfbdc2fcf2104405224b5b60
Rigo <I831003@global.corp.sap>
2016-12-29 17:11:55 -0200

    implemented day 8 part 2 solution

--- day8/screen.js
+++ day8/screen.js
@@ -14,39 +14,44 @@ rect 3x2`,
  puzzleInput
  ]
 
+var screenToString = function (screen) {
+  var screenStr = ''
+  for (var i = 0; i < 6; i++) {
+    for (var j = 0; j < 50; j++) {
+      screenStr += screen[j][i]
+    }
+    screenStr += '\n'
+  }
+  return screenStr
+}
+
+var printScreen = function (screen) {
+  console.log(screenToString(screen))
+}
+
+
+var drawRect = function (screen, w, h) {
+  for (var i = 0; i < w; i++) {
+    for (var j = 0; j < h; j++) {
+      screen[i][j] = '#'
+    }
+  }
+}
+var rotateColumn = function (screen, col, delta) {
+  //arr.push(arr.shift())
+  while(delta--) screen[col].unshift(screen[col].pop())
+}
+var rotateRow = function (screen, row, delta) {
+  while(delta--) {
+    var lastVal = screen[screen.length-1][row]
+    for (var i = screen.length - 1; i >= 1; i--) {
+      screen[i][row] = screen[i-1][row]
+    }
+    screen[0][row] = lastVal
+  }
+}
+
  var day8 = function() {
-
-  var printScreen = function (screen) {
-    var screenStr = ''
-    for (var i = 0; i < 6; i++) {
-      for (var j = 0; j < 50; j++) {
-        screenStr += screen[j][i]
-      }
-      screenStr += '\n'
-    }
-    console.log(screenStr)
-  }
-
-  var drawRect = function (screen, w, h) {
-    for (var i = 0; i < w; i++) {
-      for (var j = 0; j < h; j++) {
-        screen[i][j] = '#'
-      }
-    }
-  }
-  var rotateColumn = function (screen, col, delta) {
-    //arr.push(arr.shift())
-    while(delta--) screen[col].unshift(screen[col].pop())
-  }
-  var rotateRow = function (screen, row, delta) {
-    while(delta--) {
-      var lastVal = screen[screen.length-1][row]
-      for (var i = screen.length - 1; i >= 1; i--) {
-        screen[i][row] = screen[i-1][row]
-      }
-      screen[0][row] = lastVal
-    }
-  }
 
   for (var i = 0; i < input.length; i++) {
     var screen = []
@@ -95,11 +100,39 @@ rect 3x2`,
 var day8part2 = function() {
 
   for (var i = 0; i < input.length; i++) {
-    var sslCount = 0
+    var screen = []
+    var width = 50
+    while (width--) {
+      var height = 6
+      screen[width] = []
+      while (height--)
+      screen[width][height] = '.'
+    }
 
+    var commands = input[i].split(/\n/)
+    for (var j = 0; j < commands.length; j++) {
+      var com = commands[j].split(/\s/)
+      if (com[0] == 'rect') {
+        var size = com[1].split('x')
+        drawRect(screen, size[0], size[1])
+      } else {
+        if (com[1] == 'column') {
+          var col = com[2].split('=')[1]
+          var delta = com[4]
+          rotateColumn(screen, col, delta % 6)
+        } else {
+          var row = com[2].split('=')[1]
+          var delta = com[4]
+          rotateRow(screen, row, delta % 50)
+        }
+      }
+    }
+
+    printScreen(screen)
+    var screenStr = screenToString(screen).replace(/\n/g,'<br>')
     $('#day8part2').append(input[i])
       .append('<br>&emsp;')
-      .append(sslCount)
+      .append('<br>'+screenStr)
       .append('<br>')
   }
 
