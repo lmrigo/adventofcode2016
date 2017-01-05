a3ceacbfd27c0c344267e0fb2840029b45ea6603
Rigo <I831003@global.corp.sap>
2017-01-05 15:44:28 -0200

    implemented day 12 part 2 solution

--- day12/assembunny.js
+++ day12/assembunny.js
@@ -75,9 +75,62 @@ var day12 = function() {
 var day12part2 = function() {
 
   for (var i = 0; i < input.length; i++) {
+
+    var assembunny = {
+      // program counter
+      pc: 0,
+      // registers
+      'a': 0,
+      'b': 0,
+      'c': 1, // part 2 specific
+      'd': 0,
+      // instructions
+      'cpy': function(x, y) {
+        var numX = Number(x)
+        if (isNaN(numX)) {
+          this[y] = this[x]
+        } else {
+          this[y] = numX
+        }
+        this.pc++
+      },
+      'inc': function(x) {
+        this[x]++
+        this.pc++
+      },
+      'dec': function(x) {
+        this[x]--
+        this.pc++
+      },
+      'jnz': function(x, y) {
+        var numX = Number(x)
+        if (isNaN(numX)) {
+          numX = this[x]
+        }
+        var numY = Number(y)
+        if (isNaN(numY)) {
+          numY = this[y]
+        }
+        if (numX != 0) {
+          this.pc += numY
+        } else {
+          this.pc++
+        }
+      }
+    }
+
+    var temp = 100000000 // increased timeout for part 2
+    var program = input[i].split(/\n/)
+    while (assembunny.pc < program.length && temp > 0) {
+      var instr = program[assembunny.pc].split(/\s/)
+      assembunny[instr[0]](instr[1], instr[2])
+      temp--
+    }
+    if (!temp) console.log('timeout! #2')
+
     $('#day12part2').append(input[i])
       .append('<br>&emsp;')
-      .append()
+      .append(assembunny.a)
       .append('<br>')
   }
 
