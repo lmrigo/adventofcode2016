02dfe404c9df0b62508ba4d1cae764ff64ceee74
Rigo <lucas.rigo@sap.com>
2017-01-11 13:53:22 -0200

    implemented day 18 part 2 solution

--- day18/traps.js
+++ day18/traps.js
@@ -52,9 +52,28 @@ var day18 = function() {
 var day18part2 = function() {
   for (var i = 0; i < input.length; i++) {
 
-    $('#day18part2').append(input[i])
+    var rows = [input[i][0]]
+    var r = 0
+    var totalRows = 400000 // part 2 specific
+    while (--totalRows) {
+      var row = rows[r]
+      var newRow = ''
+      for (var t = 0; t < row.length; t++) {
+        var left = t-1 < 0 ? '.' : row[t-1]
+        var center = row[t]
+        var right = t+1 >= row.length ? '.' : row[t+1]
+        newRow += generateTile(left, center, right)
+      }
+      rows[++r] = newRow
+    }
+    // console.log(rows)
+    var safeTiles = rows.reduce(function(accum, val) {
+      return accum + val.replace(/\^/g, '').length
+    }, 0)
+
+    $('#day18part2').append(input[i][0]).append(400000)
       .append('<br>&emsp;')
-      .append()
+      .append(safeTiles)
       .append('<br>')
   }
 
